c4c4b73d621b67ef313d408f53164ee147f8e4d3
olawale-o <omoogunolawale@gmail.com>
2021-10-26 12:46:36 +0100

    Migrate Posts table

--- db/migrate/20211025100346_create_posts.rb
+++ db/migrate/20211025100346_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.references :author, null: false, foreign_key: { to_table: :users }, index: { name: 'idx_posts_user_id' }
+      t.references :user, null: false, foreign_key: true, index: true
       t.string :title, null: false, default: 'title'
       t.text :text, null: false, default: 'short text'
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_093511) do
+ActiveRecord::Schema.define(version: 2021_10_25_100346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", default: "title", null: false
+    t.text "text", default: "short text", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "comments_counter", default: 0, null: false
+    t.integer "likes_counter", default: 0, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "name", null: false
@@ -24,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_10_25_093511) do
     t.integer "posts_counter", default: 0, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
