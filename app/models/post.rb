c29bbbe664ff1be9da25c7b7477c86b8f463315e
olawale-o <omoogunolawale@gmail.com>
2021-10-31 18:05:47 +0100

    Define five_most_recent_comments in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def five_most_recent_comments(limit = 5)
+    comments.order('created_at DESC').limit(limit)
+  end
 end
