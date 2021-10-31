007f5c195bc6db13f75cdb33211874b6a72379f2
olawale-o <omoogunolawale@gmail.com>
2021-10-31 19:38:10 +0100

    Define update_post_counter method in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  def update_post_counter
+    user.update_column('posts_counter', user.posts_counter + 1)
+  end
+
   def most_recent_comments(limit = 5)
     comments.order('created_at DESC').limit(limit)
   end
