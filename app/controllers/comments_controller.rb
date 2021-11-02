94cb3912a4502f8866ca6beafa1db0583dee4577
olawale-o <omoogunolawale@gmail.com>
2021-11-02 09:16:46 +0100

    Post create comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
   def create
-    post_id = params[:id]
-    new_comment = Comment.create(user_id: current_user.id, post_id: post_id, text: comment_params[:text])
+    post = Post.find(params[:id])
+    new_comment = Comment.create(user_id: current_user.id, post_id: post.id, text: comment_params[:text])
     new_comment.update_post_comments_counter
     if new_comment.save
-      redirect_to user_post_path(current_user.id, post_id), notice: 'Comment created!'
+      redirect_to user_post_path(post.user.id, post.id), notice: 'Comment created!'
     else
       flash.now[:error] = 'Unable to create comment!'
-      redirect_to user_post_path(current_user.id, post_id), alert: 'Failed to add comment!'
+      redirect_to user_post_path(post.user.id, post.id), alert: 'Failed to add comment!'
     end
   end
 
