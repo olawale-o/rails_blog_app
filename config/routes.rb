b8ca488b47a4eea88db0b5b3b65d3231e2427fb8
olawale-o <omoogunolawale@gmail.com>
2021-11-02 09:18:45 +0100

    Create like route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
+  post 'users/:id/like/create', to: 'likes#create', as: 'post_like_create'
   post 'users/:id/comment/create', to: 'comments#create', as: 'post_comment_create'
   get 'users/:user_id/post/new', to: 'posts#new', as: 'user_post_new'
   post 'users/:user_id/post', to: 'posts#create', as: 'user_post_create'
