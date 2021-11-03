64f484aa865f2d8e46a767c98c13765c7adfeec5
olawale-o <omoogunolawale@gmail.com>
2021-11-03 14:55:11 +0100

    Migrate tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_101058) do
+ActiveRecord::Schema.define(version: 2021_11_03_135307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_101058) do
   create_table "comments", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
-    t.text "text", null: false
+    t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 2021_11_02_101058) do
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "title", null: false
-    t.text "text", null: false
+    t.string "title"
+    t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "comments_counter", default: 0, null: false
@@ -46,9 +46,9 @@ ActiveRecord::Schema.define(version: 2021_11_02_101058) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.string "photo", default: "https://i.pravatar.cc/150?img=2", null: false
-    t.text "bio", null: false
+    t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "posts_counter", default: 0, null: false
