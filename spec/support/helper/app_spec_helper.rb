aaf53b18a09f82711dc37639552044c993c763c6
olawale-o <omoogunolawale@gmail.com>
2021-11-08 20:07:19 +0100

    Create Helper module

---
+++ spec/support/helper/app_spec_helper.rb
@@ -0,0 +1,21 @@
+module Helper
+  module AppSpec
+    def capitalize(title)
+      title.capitalize
+    end
+
+    def shorten_text(text)
+      text.truncate(22)
+    end
+  end
+end
+
+# module AppSpecHelper
+#   def capitalize(title)
+#     title.capitalize
+#   end
+
+#   def shorten_text(text)
+#     text.truncate(22)
+#   end
+# end
---
+++ spec/support/helper/controller_spec_helper.rb
@@ -0,0 +1,33 @@
+module Helper
+  module ControllerSpec
+    def sigin_user
+      before(:each) do
+        @user = create(:user)
+        sign_in @user
+      end
+    end
+
+    def signin_admin
+      before(:each) do
+        @user = create(:is_admin)
+        sign_in @user
+      end
+    end
+  end
+end
+
+# module ControllerSpecHelper
+#   def sigin_user
+#     before(:each) do
+#       @user = create(:user)
+#       sign_in @user
+#     end
+#   end
+
+#   def signin_admin
+#     before(:each) do
+#       @user = create(:is_admin)
+#       sign_in @user
+#     end
+#   end
+# end
