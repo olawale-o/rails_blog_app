2032d039a44d83ff3e7a405946f5bddd35d4e4f3
olawale-o <omoogunolawale@gmail.com>
2021-11-08 20:38:08 +0100

    Fix method names

--- spec/support/helper/controller_spec_helper.rb
+++ spec/support/helper/controller_spec_helper.rb
@@ -1,13 +1,13 @@
 module Helper
   module ControllerSpec
-    def sigin_user
+    def sign_in_user
       before(:each) do
         @user = create(:user)
         sign_in @user
       end
     end
 
-    def signin_admin
+    def sign_in_admin
       before(:each) do
         @user = create(:is_admin)
         sign_in @user
