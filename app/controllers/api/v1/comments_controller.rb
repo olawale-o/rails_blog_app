6d063bf242c219b06a106174780272d97a22d5e4
olawale-o <omoogunolawale@gmail.com>
2021-11-10 10:21:29 +0100

    Create api comments_controller

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,33 @@
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      load_and_authorize_resource
+      def index
+        puts current_user.name
+        post_id = params[:post_id]
+        @comments = Comment.where({ post_id: post_id }).order(:created_at)
+        render json: {
+          success: true, message: "Loaded all comments for post #{post_id}", data: { comments: @comments }
+        }
+      end
+
+      def create
+        puts current_user.name
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.new(text: comment_params[:text], user_id: current_user.id)
+
+        if @comment.save
+          render json: { success: true, data: { comment: @comment } }, status: :created
+        else
+          render json: { success: false, errors: @comment.errors }, status: :bad_request
+        end
+      end
+
+      private
+
+      def comment_params
+        params.require(:comment).permit(:text)
+      end
+    end
+  end
+end
