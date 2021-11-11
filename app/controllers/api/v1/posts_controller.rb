8f9789004d83c55af353557a8dd4383c51cd74d9
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-11 17:07:29 +0200

    Add authorizations to api posts controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class PostsController < ApplicationController
+      load_and_authorize_resource
+
       def index
         @posts = Post.order(:created_at)
         render json: { success: true, message: 'Loaded all posts', data: { posts: @posts } }
